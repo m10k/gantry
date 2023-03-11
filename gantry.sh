c0f6a1418a9a786a1ac6a28255cd8471d539993b
Matthias Kruk <m@m10k.eu>
2023-03-11 18:16:35 +0900

    gantry: Announce new images and errors via pubsub messages
    Gantry does not provide any feedback other than its return value,
    leaving it up to the caller to integrate it with other applications.
    
    This commit modifies gantry so that it announces images and errors
    using pubsub messaging if the caller has specified topics for such
    announcements on the command line.

--- gantry.sh
+++ gantry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2022, Matthias Kruk
+# Copyright (c) 2022-2023, Matthias Kruk
 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -24,6 +24,78 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+current_timestamp() {
+	date +"%Y-%m-%d.%H:%M:%S%z"
+}
+
+announce() {
+	local topic="$1"
+	local message="$2"
+
+	local endpoint
+	local -i error
+
+	if ! endpoint=$(ipc_endpoint_open); then
+		return 1
+	fi
+
+	ipc_endpoint_publish "$endpoint" "$topic" "$message"
+	error="$?"
+
+	ipc_endpoint_close "$endpoint"
+	return "$error"
+}
+
+announce_error() {
+	local error="$1"
+	local log="$2"
+
+	local topic
+
+	topic=$(opt_get "errors-to")
+
+	if [[ -n "$topic" ]]; then
+		local msg
+		local timestamp
+
+		if ! timestamp=$(current_timestamp); then
+			log_warn "Could not get a timestamp"
+		fi
+
+		msg=$(json_object "type"      "gantry-error" \
+		                  "error"     "$error"       \
+		                  "timestamp" "$timestamp"   \
+		                  "log"       "$log")
+		announce "$topic" "$msg"
+	fi
+
+	return 0
+}
+
+announce_image() {
+	local image="$1"
+
+	local topic
+
+	topic=$(opt_get "announce-to")
+
+	if [[ -n "$topic" ]]; then
+		local msg
+		local timestamp
+
+		if ! timestamp=$(current_timestamp); then
+			log_warn "Could not get a timestamp"
+		fi
+
+		msg=$(json_object "type"      "gantry-push" \
+		                  "image"     "$image"      \
+		                  "timestamp" "$timestamp")
+		announce "$topic" "$msg"
+	fi
+
+	return 0
+}
+
 make_image() {
 	local user="$1"
 	local image="$2"
@@ -37,6 +109,7 @@ make_image() {
 	                           --distro "$distro" 2>&1); then
 		log_error "Image generation failed"
 		log_highlight "stuffer output" <<< "$output" | log_error
+		announce_error "Image generation failed" "$output"
 		return 1
 	fi
 
@@ -53,7 +126,10 @@ push_image() {
 	if ! output=$(docker push "$user/$image:$version" 2>&1); then
 		log_error "Failed to push image to hub.docker.com"
 		log_highlight "docker push output" <<< "$output" | log_error
+		announce_error "Image push failed" "$output"
 		return 1
+	else
+		announce_image "$user/$image:$version"
 	fi
 
 	return 0
@@ -86,9 +162,12 @@ main() {
 	local tag
 	local distro
 
-	opt_add_arg "u" "user"       "rv" "" "hub.docker.com username"
-	opt_add_arg "t" "tag"        "rv" "" "Tag of the image"            '^[^:]+:.+$'
-	opt_add_arg "d" "distro"     "rv" "" "Distro of the new image"
+	opt_add_arg "u" "user"        "rv" "" "hub.docker.com username"
+	opt_add_arg "t" "tag"         "rv" "" "Tag of the image"               \
+	            '^[^:]+:.+$'
+	opt_add_arg "d" "distro"      "rv" "" "Distro of the new image"
+	opt_add_arg "A" "announce-to" "v"  "" "Pubsub topic for announcements"
+	opt_add_arg "E" "errors-to"   "v"  "" "Pubsub topic for errors"
 
 	if ! opt_parse "$@"; then
 		return 1
@@ -107,7 +186,7 @@ main() {
 
 {
 	if ! . toolbox.sh ||
-	   ! include "log" "opt"; then
+	   ! include "log" "opt" "json" "uipc"; then
 		exit 1
 	fi
 
